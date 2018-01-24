46149910e11c64ec76a9c3c3b6d5690e29bdbdd5
carlos <carlosbarreraojeda@gmail.com>
2018-01-24 12:08:27 -0600

    this might work

---
+++ web/TeamActivity03/Team03.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Team03</title>
+</head>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+<h2>i dont think the php is working, because it's a php file</h2>
+</body>
+</html>
