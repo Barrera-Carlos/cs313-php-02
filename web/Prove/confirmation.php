5dbee5732b2083a78db26b2c1ad43f9f9e49cd3e
carlos <carlosbarreraojeda@gmail.com>
2018-01-27 16:56:51 -0600

    wabba lubba dub dub

--- web/Prove/confirmation.php
+++ web/Prove/confirmation.php
@@ -22,63 +22,68 @@ session_start();
 
 $var = $_SESSION["list"];
 
+    echo "<h1 style='text-align: center'>Final sale</h1>";
     if (empty($_POST["name"])) {
-        echo "<h3>name is required</h3><br>";
+        echo "<h3 style='color: red; text-align: center'>name is required</h3><br>";
     }
     else {
         $name = $_POST["name"];
         if (!preg_match("/^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/", $name)) {
-            echo "<h3>Only letters and white space allowed</h3><br>";
+            echo "<h3 style='color: red; text-align: center'>Only letters and white space allowed</h3><br>";
         }
         else{
-            echo "<h3>".$name."</h3><br>";
+            echo "<h3 style='text-align: center'>".$name."</h3><br>";
         }
     }
 
     if (empty($_POST["address"])) {
-        echo "<h3>address is required</h3><br>";
+        echo "<h3 style='color: red; text-align: center'>address is required</h3><br>";
     }
     else {
         $address = $_POST["address"];
         if (preg_match("/^[A-Za-z0-9 ]*$/", $address)) {
-            echo "<h3>Only letters and numbers are allowed</h3><br>";
+            echo "<h3 style='color: red ; text-align: center'>Only letters and numbers are allowed</h3><br>";
         }
         else{
-            echo "<h3>".$address."</h3><br>";
+            echo "<h3 style='text-align: center'>".$address."</h3><br>";
         }
     }
 
     if (empty($_POST["city"])) {
-        echo "<h3>City is require</h3><br>";
+        echo "<h3 style='color: red; text-align: center'>City is require</h3><br>";
     }
     else {
         $city = $_POST["city"];
         if (!preg_match("/^[a-zA-Z ]*$/", $city)) {
-            echo "<h3>Only letters and white space allowed</h3><br>";
+            echo "<h3 style='color: red ; text-align: center'>Only letters and white space allowed</h3><br>";
         }
         else{
-            echo "<h3>".$city."</h3><br>";
+            echo "<h3 style='text-align: center'>".$city."</h3><br>";
         }
     }
 
     if (empty($_POST["state"])) {
-        echo "<h3>City is require</h3><br>";
+        echo "<h3 style='color: red; text-align: center'>City is require</h3><br>";
     }
     else {
         $state = $_POST["state"];
         if (!preg_match("/^[a-zA-Z ]*$/", $state)) {
-            echo "<h3>Only letters and white space allowed</h3><br>";
+            echo "<h3 style='color: red ; text-align: center'>Only letters and white space allowed</h3><br>";
         }
         else{
-            echo "<h3>".$state."</h3><br>";
+            echo "<h3 style='text-align: center'>".$state."</h3><br>";
         }
     }
 
-    $zip = $_POST["zip"];
-    echo "<h3>".$zip."</h3><br>";
+    if (empty($_POST["zip"])) {
+        echo "<h3 style='color: red; text-align: center'>zip is require</h3><br>";
+    }
+    else {
+        echo "<h3 style='text-align: center'>".$zip."</h3><br>";
+    }
 
     foreach ($var as $item){
-        echo "<h3>".$item[1]."</h3><br>";
+        echo "<h3 style='text-align: center'>".$item[1]."</h3><br>";
     }
 
 
