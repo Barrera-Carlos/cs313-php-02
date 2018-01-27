5762d137ee5ad82d4e34509a6a8e0295fc3c64d0
carlos <carlosbarreraojeda@gmail.com>
2018-01-27 02:58:33 -0600

    what

--- web/Prove/shop.php
+++ web/Prove/shop.php
@@ -59,10 +59,7 @@ session_start();
   </nav>
 
 <?php
-/*need to make this into a multi dimensional array to add a total to the shopping cart
-$itemDesctiption = array( "10" => "Gundam nadleeh $12.50", "2"=>"Gundam Exia $15.66",
-"3" =>"Transient Gundam $23.99", "4"=>"Transient Gundam Glacier $23.99", "7"=>" Gundam Vidar 18.50",
- "5"=>"Kimaris vidar $23.99");*/
+
 
 $itemDesctiption = array(
         array("10","Gundam nadleeh $12.50",12.50),
@@ -73,18 +70,28 @@ $itemDesctiption = array(
         array("5","Kimaris vidar $23.99",23.99));
 
 
-if (is_array($_POST['Item'])) {
-    /*$_SESSION["totalPrice"] = 0;*/
-    $gundamIdNumberLocation = 0;
-    $gundamDescriptionLocation = 1;
-    $gundamPriceLocation = 2;
-    $totalPriceVar = 0;
-    $checkoutArray = array();
-    $removedID = "";
-    $initialArray = array();
-    $_SESSION["list"];
 
-    echo "<form action=\"checkout.php\" method='post'>";
+$gundamIdNumberLocation = 0;
+$gundamDescriptionLocation = 1;
+$gundamPriceLocation = 2;
+$totalPriceVar = 0;
+$checkoutArray = array();
+$removedID = "";
+$initialArray = array();
+#$_SESSION["list"];
+
+
+echo "<form action=\"checkout.php\" method='post'>";
+if(isset($_SESSION["list"])){
+    echo "goth boy click";
+    $structure = $_SESSION["list"];
+    foreach ($structure as $item){
+        array_push($initialArray,$item);
+        $id = $item[$gundamIdNumberLocation];
+        echo "<input type='checkbox' name=\"Remove[]\" value=\"$id\">".$item[$gundamDescriptionLocation]."<br>";
+    }
+}
+else{
     foreach($_POST['Item'] as $value){
         for($row = 0; $row < 6; $row++){
             if($value == $itemDesctiption[$row][$gundamIdNumberLocation])
@@ -92,32 +99,13 @@ if (is_array($_POST['Item'])) {
                 array_push($initialArray,$itemDesctiption[$row]);
                 $id = $itemDesctiption[$row][$gundamIdNumberLocation];
                 echo "<input type='checkbox' name=\"Remove[]\" value=\"$id\">".$itemDesctiption[$row][$gundamDescriptionLocation]."<br>";
-
             }
         }
     }
-    $_SESSION["list"] = $initialArray;
-    echo "<input type='submit'></form>";
+}
+$_SESSION["list"] = $initialArray;
+echo "<input type='submit'></form>";
 
-    /*echo "<ul id=\"UlistStyle\">";
-    foreach($_POST['Item'] as $value){
-        for($row = 0; $row < 6; $row++){
-            if($value == $itemDesctiption[$row][$gundamIdNumberLocation])
-            {
-                 array_push($initialArray,$itemDesctiption[$row]);
-
-                echo "<li id=\"ListStyle\">".$itemDesctiption[$row][$gundamDescriptionLocation].
-                    "<button onclick=\" remove(this.parentNode)\">Remove this Item</button></li>";
-
-                $_SESSION["list"] = $initialArray;
-            }
-        }
-
-      }
-        echo "</ul>";*/
-  }
-
-/*echo "<a href=\"checkout.php\">check out</a>";*/
 ?>
 
 </body>
