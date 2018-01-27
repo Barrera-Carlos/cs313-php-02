83fc950eecc0fb156f86f78ea028673c819a1a76
carlos <carlosbarreraojeda@gmail.com>
2018-01-27 02:07:12 -0600

    testing checkout

--- web/Prove/checkout.php
+++ web/Prove/checkout.php
@@ -12,14 +12,13 @@ $var = $_SESSION["list"];
 $wantedItems = array();
 
 foreach ($_POST['Remove'] as $item){
-    echo "<h1>".$item."</h1><br>";
-    /*foreach ($var as $value){
+    foreach ($var as $value){
         if($item == $value[0]){
             array_push($wantedItems,$value);
             echo "<h1>".$value[1]."</h1><br>";
         }
-    }*/
-    #$_SESSION["list"] = $wantedItems;
+    }
+    $_SESSION["list"] = $wantedItems;
 }
 
 ?>
