cf105f9954581cd27d5374ec85f78568138cdc0a
carlos <carlosbarreraojeda@gmail.com>
2018-01-27 03:22:07 -0600

    work

--- web/Prove/checkout.php
+++ web/Prove/checkout.php
@@ -41,23 +41,25 @@ session_start();
 
 <?php
 // Echo session variables that were set on previous page
-$var = $_SESSION["list"];
-$wantedItems = array();
-$itemWanted = true;
-
-foreach ($var as $value){
+if(isset($_POST['remove'])){
+    $var = $_SESSION["list"];
+    $wantedItems = array();
     $itemWanted = true;
-    foreach ($_POST['Remove'] as $item){
-        if($value[0] == $item){
-            $itemWanted = false;
+
+    foreach ($var as $value){
+        $itemWanted = true;
+        foreach ($_POST['Remove'] as $item){
+            if($value[0] == $item){
+                $itemWanted = false;
+            }
+        }
+        if($itemWanted){
+            array_push($wantedItems,$value);
+            echo "<h1>".$value[1]."</h1><br>";
         }
     }
-    if($itemWanted){
-        array_push($wantedItems,$value);
-        echo "<h1>".$value[1]."</h1><br>";
-    }
+    $_SESSION["list"] = $wantedItems;
 }
-$_SESSION["list"] = $wantedItems;
 
 ?>
 <a href="shop.php">cart</a>
--- web/Prove/shop.php
+++ web/Prove/shop.php
@@ -48,41 +48,43 @@ $itemDesctiption = array(
         array("5","Kimaris vidar $23.99",23.99));
 
 
-
-$gundamIdNumberLocation = 0;
-$gundamDescriptionLocation = 1;
-$gundamPriceLocation = 2;
-$totalPriceVar = 0;
-$checkoutArray = array();
-$removedID = "";
-$initialArray = array();
+if(isset($_POST['Item'])){
+    $gundamIdNumberLocation = 0;
+    $gundamDescriptionLocation = 1;
+    $gundamPriceLocation = 2;
+    $totalPriceVar = 0;
+    $checkoutArray = array();
+    $removedID = "";
+    $initialArray = array();
 #$_SESSION["list"];
 
 
-echo "<form action=\"checkout.php\" method='post'>";
-if(isset($_SESSION["list"])){
-    echo "ninja";
-    $structure = $_SESSION["list"];
-    foreach ($structure as $item){
-        array_push($initialArray,$item);
-        $id = $item[$gundamIdNumberLocation];
-        echo "<input type='checkbox' name=\"Remove[]\" value=\"$id\">".$item[$gundamDescriptionLocation]."<br>";
+    echo "<form action=\"checkout.php\" method='post'>";
+    if(isset($_SESSION["list"])){
+        echo "ninja<br>";
+        $structure = $_SESSION["list"];
+        foreach ($structure as $item){
+            array_push($initialArray,$item);
+            $id = $item[$gundamIdNumberLocation];
+            echo "<input type='checkbox' name=\"Remove[]\" value=\"$id\">".$item[$gundamDescriptionLocation]."<br>";
+        }
     }
-}
-else{
-    foreach($_POST['Item'] as $value){
-        for($row = 0; $row < 6; $row++){
-            if($value == $itemDesctiption[$row][$gundamIdNumberLocation])
-            {
-                array_push($initialArray,$itemDesctiption[$row]);
-                $id = $itemDesctiption[$row][$gundamIdNumberLocation];
-                echo "<input type='checkbox' name=\"Remove[]\" value=\"$id\">".$itemDesctiption[$row][$gundamDescriptionLocation]."<br>";
+    else{
+        foreach($_POST['Item'] as $value){
+            for($row = 0; $row < 6; $row++){
+                if($value == $itemDesctiption[$row][$gundamIdNumberLocation])
+                {
+                    array_push($initialArray,$itemDesctiption[$row]);
+                    $id = $itemDesctiption[$row][$gundamIdNumberLocation];
+                    echo "<input type='checkbox' name=\"Remove[]\" value=\"$id\">".$itemDesctiption[$row][$gundamDescriptionLocation]."<br>";
+                }
             }
         }
     }
+    $_SESSION["list"] = $initialArray;
+    echo "<input type='submit'></form>";
+
 }
-$_SESSION["list"] = $initialArray;
-echo "<input type='submit'></form>";
 
 ?>
 
