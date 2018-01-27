a980a7e139ca28763309c03cc32c8b606992c031
carlos <carlosbarreraojeda@gmail.com>
2018-01-27 02:31:32 -0600

    testing checkout

--- web/Prove/checkout.php
+++ web/Prove/checkout.php
@@ -10,16 +10,34 @@ session_start();
 // Echo session variables that were set on previous page
 $var = $_SESSION["list"];
 $wantedItems = array();
+$itemWanted = true;
 
-foreach ($_POST['Remove'] as $item){
+foreach ($var as $value){
+    $itemWanted = true;
+    foreach ($_POST['Remove'] as $item){
+        if($value[0] == $item){
+            $itemWanted = false;
+        }
+    }
+    if($itemWanted){
+        array_push($wantedItems,$value);
+        echo "<h1>".$value[1]."</h1><br>";
+    }
+}
+$_SESSION["list"] = $wantedItems;
+/*foreach ($_POST['Remove'] as $item){
     foreach ($var as $value){
         if($item == $value[0]){
+            $itemNotWanted = true;
             array_push($wantedItems,$value);
             echo "<h1>".$value[1]."</h1><br>";
         }
+        if (!$itemNotWanted){
+
+        }
     }
     $_SESSION["list"] = $wantedItems;
-}
+}*/
 
 ?>
 
